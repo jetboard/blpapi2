db7d1cff1ef40303547f68f5d4643785ce4d9868
jetboard <117104255+jetboard@users.noreply.github.com>
2022-11-08 11:17:08 -0600

    Add files via upload

---
+++ version.py
@@ -0,0 +1,34 @@
+# version.py
+
+"""Provide BLPAPI SDK versions"""
+
+from . import versionhelper
+
+__version__ = "3.19.1"
+
+
+def print_version() -> None:
+    """Print version information of BLPAPI python module and blpapi C++ SDK"""
+    print("Python BLPAPI SDK version: ", version())
+    print("C++ BLPAPI SDK version:    ", cpp_sdk_version())
+
+
+def version() -> str:
+    """
+    Returns:
+        str: BLPAPI Python module version
+    """
+    return __version__
+
+
+def cpp_sdk_version() -> str:
+    """
+    Returns:
+        str: BLPAPI C++ SDK dependency version
+    """
+    version_string = ".".join(map(str, versionhelper.blpapi_getVersionInfo()))
+
+    commit_id = versionhelper.blpapi_getVersionIdentifier()
+    if commit_id != "Unknown":
+        version_string += " (" + commit_id + ")"
+    return version_string
